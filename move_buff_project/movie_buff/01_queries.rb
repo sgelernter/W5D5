b0d867df358dce2867741ec078e110ab6549f044
Mackinjosh <mackzumarraga@gmail.com>
2021-11-05 12:16:35 -0400

    Started Movie Buff Project

--- move_buff_project/movie_buff/01_queries.rb
+++ move_buff_project/movie_buff/01_queries.rb
@@ -6,7 +6,7 @@ def it_was_ok
   # We can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
-
+  Movie.select(:id, :title, :score).where(score: 2..3)
 end
 
 def harrison_ford
@@ -20,7 +20,10 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
-
+  Actor
+    .select(:id, :title)
+    .joins(:movies)
+    .where("name = ? AND NOT ord = ?", "Harrison Ford", 1)
 end
 
 def biggest_cast
